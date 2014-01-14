5452fdcbfbfe9f234a6f6482b0570f3e9626d2b4
OlivierDouangvichith <Olivier.Douangvichith@gmail.com>
2014-01-14 18:32:06 +0000

    Initial commit
    Mes1001Choses

---
+++ server.js
@@ -0,0 +1,4 @@
+var americano = require('americano');
+
+var port = process.env.PORT || 9150;
+americano.start({name: 'Mes1001Choses', port: port});
---
+++ server/controllers/routes.js
@@ -0,0 +1,11 @@
+/* 
+* Set the routes of your app here.
+*/ 
+ReceiptDetails = require('./receiptdetails');
+
+module.exports = {
+  'receiptdetails': {
+      get: ReceiptDetails.list
+  }
+};
+
