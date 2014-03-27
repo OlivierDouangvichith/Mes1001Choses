c73613b2122d9a15e84dc703efc4487f64fbc574
OlivierDouangvichith <Olivier.Douangvichith@gmail.com>
2014-03-27 19:08:51 +0000

    test encore

--- server/controllers/geolocationlogs.js
+++ server/controllers/geolocationlogs.js
@@ -149,7 +149,7 @@ module.exports.api = function(req, res) {
             console.log('API /public username ou token non valide');
             data_out = {
                 code:'KO',
-                label:'API /public username ou token non valide',   
+                label:'API /public username ou token non valide mmucSession='+mmucSession,   
                 timestamp:Math.round(+new Date()/1000),
                 url:req.host,
                 query:srvUrl.query,
